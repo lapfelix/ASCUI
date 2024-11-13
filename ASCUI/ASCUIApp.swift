8a8a79c32a0a6ed1c16934b4f07251c53442890c
lapfelix <felix@lapal.me>
2024-11-13 17:27:45 -0500

    Initial Commit

---
+++ ASCUI/ASCUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASCUIApp.swift
+//  ASCUI
+//
+//  Created by Felix Lapalme on 2024-11-13.
+//
+
+import SwiftUI
+
+@main
+struct ASCUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ASCUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ASCUI
+//
+//  Created by Felix Lapalme on 2024-11-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
